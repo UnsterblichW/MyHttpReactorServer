098087994feb9a1a0236b88ab734ed70a02265ff
SHADOW <unsterblich.wang@gmail.com>
2024-06-25 00:18:51 +0800

    提供另一种输出日志的方式，直接输出到控制台

--- src/log/log.h
+++ src/log/log.h
@@ -60,14 +60,23 @@ private:
     std::mutex mtx_;
 };
 
-#define LOG_BASE(level, format, ...) \
+// 这个LOG_BASE可以把信息输出到控制台，看起来比较方便
+#define LOG_BASE(type, fmt, args...)  \
+  do{\
+    printf("%d: %s@%s, line: %d\n***LogInfo[", type, __FILE__, __FUNCTION__, __LINE__);\
+    printf(fmt, ##args);\
+    printf("]\n\n");\
+  }while(0);
+
+// 这个LOG_BASE可以把日志写到文件里面去，只是为了技术实践
+/* #define LOG_BASE(level, format, ...) \
     do {\
         Log* log = Log::Instance();\
         if (log->IsOpen() && log->GetLevel() <= level) {\
             log->write(level, format, ##__VA_ARGS__); \
             log->flush();\
         }\
-    } while(0);
+    } while(0); */
 
 #define LOG_DEBUG(format, ...) do {LOG_BASE(0, format, ##__VA_ARGS__)} while(0);
 #define LOG_INFO(format, ...) do {LOG_BASE(1, format, ##__VA_ARGS__)} while(0);
